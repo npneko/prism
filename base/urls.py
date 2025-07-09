19bcb7466f0afdc7d155f53bf4ceccd040daa5b3
npneko <221901489+npneko@users.noreply.github.com>
2025-07-10 01:10:12 +0200

    User registration

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, CustomLogoutView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, CustomLogoutView, RegisterPage
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", CustomLogoutView.as_view(http_method_names=['get', 'post', 'options']), name="logout"),
+    path("register/", RegisterPage.as_view(), name="register"),
     path("", TaskList.as_view(), name="tasks"),
     path("task/<int:pk>", TaskDetail.as_view(), name="task"),
     path("task-create", TaskCreate.as_view(), name="task-create"),
